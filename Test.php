d3421e1b9886b0a0fece03661dacb0575cd09e78
Gregory-febvin <70692941+Gregory-febvin@users.noreply.github.com>
2021-06-07 16:32:41 +0200

    Update Test.php

--- Test.php
+++ Test.php
@@ -105,3 +105,29 @@ Bon courage.');
 INSERT INTO `livredor` VALUES ('13','Prune','Oh lalalala !
 Le boooo site ! ;o)');
 */
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>  
+        <?php
+            $servername = 'localhost';
+            $username = 'root';
+            $password = 'root';
+            
+            //On établit la connexion
+            $conn = mysqli_connect($servername, $username, $password);
+            
+            //On vérifie la connexion
+            if(!$conn){
+                die('Erreur : ' .mysqli_connect_error());
+            }
+            echo 'Connexion réussie';
+        ?>
+    </body>
+</html>
